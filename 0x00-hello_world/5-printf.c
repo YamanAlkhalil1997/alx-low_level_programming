0f6b682c2a140eb3efcb1a16332ac6f51ad34656
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-09-07 19:42:14 -0700

    asd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description : A C program that prints with printf function
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	pfintf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
