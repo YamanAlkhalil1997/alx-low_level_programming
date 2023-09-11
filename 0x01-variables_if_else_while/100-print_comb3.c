a2468bbb8e2509c515115c8fc00f7424bac6f814
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-09-11 16:42:34 -0700

    a

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,26 +12,34 @@
 int main(void)
 {
 	int digit1 = 0, digit2;
-
 	while (digit1 <= 9)
 	{
-		if (digit1 != digit2 && digit1 < digit2)
+		digit2 = 0;
+		while (digit2 <= 9)
 		{
-			putchar(digit1 + 48);
-			putchar(digit2 + 48);
-
-			if (digit1 + digit2 != 17)
+			if (digit1 != digit2 && digit1 < digit2)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
+
+				if (digit1 + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+
+				}
+
 
 			}
 			digit2++;
+
 		}
 		digit1++;
-
+	
+	
+	
 	}
-	putchar('\n');
+	putchar ('\n');
 
 	return (0);
 }
