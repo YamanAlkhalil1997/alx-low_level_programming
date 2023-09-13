480a29b3cbca895d22a8cf6e37d5238cedbdffdf
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-09-12 17:22:58 -0700

    as

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  *	write(int fd,const void *buf,size_t count)
  *
  * return 1 (not success)
- */
+*/
 
 int main(void)
 {	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
