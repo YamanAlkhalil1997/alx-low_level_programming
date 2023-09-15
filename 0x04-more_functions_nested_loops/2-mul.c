4e4d7724889e5910c6f0a1794b497bb31759a330
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-09-14 19:12:01 -0700

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if c is upper
+ *
+ * @c imput for alphabet
+ *
+ * return: 1 if its uppercase, 0 if not
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
