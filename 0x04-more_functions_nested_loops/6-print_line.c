64bcf9ad62924b69a9808efbd48b89b52045c9d5
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-09-14 19:53:42 -0700

    A

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int print_line(int n)
+{
+	int lnChr;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lnChr = 1; lnChr <= lnChr++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
