fbb72b37c2fbfc8af60cc60863f89badef7a97d2
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-09-20 19:06:50 -0700

    A

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * islower - determines whether ascil is lowercase
+ * @c: character
+ * return: 1 if true, 0 if false
+*/
+
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter - determines wbether ascil is a delimiter
+ * @c: character
+ * return 1 if true. 0 if false
+*/
+
+int isDelimiter(char c)
+{
+	int i;
+	char delimiters[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
+}
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string
+ * return: string with capitalized words
+*/
+
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimit = 1;
+
+	while (*s)
+	{
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit = 0;
+		}
+		else
+			foundDelimit = 0;
+		s++;
+	}
+	return (ptr);
+}
