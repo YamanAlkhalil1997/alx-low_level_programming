86ee5ca80ad5a4fba7ef1e3f71ce6fd5029bd195
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-10-02 19:48:54 -0700

    A

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - print
+ * @argc: int
+ * @arvg: list
+ * return 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (argc --)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
