545924f52ea794b653c5827a423a5eb8819b1ff5
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-10-04 02:23:28 -0700

    A

---
+++ 0x09-static_libraries/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-print sim of 2 numbers
+ * @argc:number
+ * @argv:pointer
+ * return:0
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum;
+	
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
