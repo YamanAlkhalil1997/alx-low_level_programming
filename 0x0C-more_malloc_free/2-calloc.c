d0ed0f4fa616971ce24f1f2e2c79f61dd2d9f440
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-10-08 16:23:39 -0700

    A

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * *_memst - fills memory with a constact
+ * @s: pointer to put the cosn
+ * @b: scontant
+ * @n: max bytes to use
+ * return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+
+	return (ptr);
+}
+/**
+ * *_calloc - allocates memlory for an array
+ * @nmemb: array length
+ * @size: size of each element
+ * return: pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL)
+	m = malloc(sizeof(int) * nmemb);
+
+	if (m == 0)
+		return (NULL);
+
+	_memset(m, 0, sizeof(int) * nmemb);
+
+	return (m);
+}
