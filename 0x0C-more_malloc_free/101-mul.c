7dd6f28be6f52a29e47b62f46af69749dde9aeee
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-10-08 18:07:24 -0700

    A

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -30,7 +30,7 @@ void _puts(char *str)
 int _atoi(const char *s)
 {
 	int sign = 1;
-		unsigned long int resp = 0, firstNum, i;
+	unsigned long int resp = 0, firstNum, i;
 
 		for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 		{
@@ -60,6 +60,7 @@ void print_int(unsigned long int n)
 
 	for (i = 0; n / divisor > 9; i++, divisor *= 10)
 	;
+
 	for (; divisor >= 1; n %= divisor, divisor /= 10)
 	{
 		resp = n / divisor;
