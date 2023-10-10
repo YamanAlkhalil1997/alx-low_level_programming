02d20141829a4d77436192853a954beab9f8c844
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-10-10 16:04:49 -0700

    A

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct asd asd asd
+ * @name: dods name
+ * @age: dogs age
+ * @owner: 
+ *
+ * descriotion: just a long struct in a big kitty wolrd
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
