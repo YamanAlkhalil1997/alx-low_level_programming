ef86531d594d659d706e392e152020f9e8b08f08
yamanalkhalil1997 <yamanalkhalil161@gmail.com>
2023-10-10 17:00:14 -0700

    A

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -47,6 +47,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if ((*dog).name == NULL)
 	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*dog).name == NULL)
+	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
